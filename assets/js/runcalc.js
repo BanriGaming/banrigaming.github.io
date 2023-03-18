fb1f9d84f312f7b188abdb34b64be1ab43576983
Bhumika-P <bhumika.9117@gmail.com>
2023-03-18 14:05:23 -0500

    Melvor idle calculations

--- assets/js/runcalc.js
+++ assets/js/runcalc.js
@@ -1,37 +1,55 @@
 
+function cleardata(){ 
+    document.getElementById("title").innerHTML = "";
+    document.getElementById("category").innerHTML = "";
+    document.getElementById("level").innerHTML = "";
+    document.getElementById("runes").innerHTML="";
+    document.getElementById("c_runes").innerHTML="";
+    document.getElementsByTagName("h2").innerHTML="";
+    document.getElementsByTagName("h3").innerHTML="";
+}
 function calculateRunes(){
-    document.getElementById("showdata").style.display="block";
+    cleardata();
+    document.getElementById("showdata").style.display="block";  
     var cast = document.getElementById("cast").value; //Number of casts
     var magic = document.getElementById("magic").value; //Magic title
     var main_div = document.getElementById("showdata");
+    var rune_node = document.getElementById("runes");
+    var c_rune_node = document.getElementById("c_runes");
     //Parse json file
     $.getJSON("https://banrigaming.github.io/assets/json/runes.json", function(json) {
         calc(json); // this will show the info it in firebug console
     });
     function calc(json){
-        console.log(json.runes.length);
         for(i=0 ; i<json.runes.length ; i++){
             if(magic == json.runes[i].title){
-                console.log("here");
-                const node = document.createElement("p");
-                const textnode = document.createTextNode("Water");
-                node.appendChild(textnode);
-                main_div.appendChild(node);
-                console.log(json.runes[i].combination);
-                var text="";
-               /* for (const x in json.runes[i].combination) {
-                    text += json.runes[i].combination[x] + ", ";
-                    console.log(x);
-                    console.log(json.runes[i].combination[x]);
-                }*/
-                console.log(text);
-                for (var key in json.runes[i].combination) {
-                    console.log(key);
-                    console.log(json.runes[i].combination[key]);
+                document.getElementById("title").innerHTML = json.runes[i].title;
+                document.getElementById("category").innerHTML = "Category: "+ json.runes[i].type;
+                document.getElementById("level").innerHTML = "Level: "+json.runes[i].level;
+                for (var key in json.runes[i].standard) {
+                    const rune_text = document.createTextNode(key.charAt(0).toUpperCase()+key.slice(1)+" Runes: ");
+                    const total_text = document.createTextNode(json.runes[i].standard[key]*cast+" ");
+                    rune_node.appendChild(rune_text);
+                    rune_node.appendChild(total_text);
+                    main_div.appendChild(rune_node);
                 }
+                console.log(json.runes[i].options);
+                if(json.runes[i].options === true){ //for combination
+                    c_rune_node.style.display="block"; 
+                    for (var key in json.runes[i].combination) {
+                        const rune_text = document.createTextNode(key.charAt(0).toUpperCase()+key.slice(1)+" Runes: ");
+                        const total_text = document.createTextNode(json.runes[i].combination[key]*cast+" ");
+                        c_rune_node.appendChild(rune_text);
+                        c_rune_node.appendChild(total_text);
+                        main_div.appendChild(c_rune_node);
+                    }
+                }else{
+                    //c_rune_node.style.display="none";
+                }
+                
             }
         }
     }
-
+    //document.getElementById("showdata").innerHTML="";
     
 }
