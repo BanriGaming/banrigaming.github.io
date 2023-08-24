70f4a2a8a1bef8bc68fc3ecb2664476abe6050cf
BanriGaming <monkguru.guardian@gmail.com>
2023-08-24 13:01:16 -0500

    Palia Gifts Page test 1

--- assets/js/villagers.js
+++ assets/js/villagers.js
@@ -2,37 +2,40 @@ const villagerSelect = document.getElementById('villagerSelect');
 const likesList = document.getElementById('likesList');
 const lovesList = document.getElementById('lovesList');
 
+// Load villagers from JSON
 fetch('villagers.json')
-  .then(response => response.json())
-  .then(data => {
-    for (const villager in data) {
-      const option = document.createElement('option');
-      option.value = villager;
-      option.textContent = capitalizeFirstLetter(villager);
-      villagerSelect.appendChild(option);
+    .then(response => response.json())
+    .then(villagers => {
+        console.log('Villagers loaded:', villagers);
+        
+        for (const villager in villagers) {
+            const option = document.createElement('option');
+            option.value = villager;
+            option.textContent = villager.charAt(0).toUpperCase() + villager.slice(1);
+            villagerSelect.appendChild(option);
+        }
+    })
+    .catch(error => {
+        console.error('Error fetching villagers:', error);
+    });
+
+villagerSelect.addEventListener('change', () => {
+    const selectedVillager = villagerSelect.value;
+    if (selectedVillager) {
+        const villager = villagers[selectedVillager];
+        likesList.innerHTML = '';
+        lovesList.innerHTML = '';
+
+        for (const like of villager.likes) {
+            const li = document.createElement('li');
+            li.textContent = like;
+            likesList.appendChild(li);
+        }
+
+        for (const love of villager.loves) {
+            const li = document.createElement('li');
+            li.textContent = love;
+            lovesList.appendChild(li);
+        }
     }
-  });
-
-villagerSelect.addEventListener('change', function() {
-  const selectedVillager = villagerSelect.value;
-  const selectedVillagerData = villagers[selectedVillager];
-
-  likesList.innerHTML = '';
-  lovesList.innerHTML = '';
-
-  selectedVillagerData.likes.forEach(item => {
-    const listItem = document.createElement('li');
-    listItem.textContent = item;
-    likesList.appendChild(listItem);
-  });
-
-  selectedVillagerData.loves.forEach(item => {
-    const listItem = document.createElement('li');
-    listItem.textContent = item;
-    lovesList.appendChild(listItem);
-  });
 });
-
-function capitalizeFirstLetter(string) {
-  return string.charAt(0).toUpperCase() + string.slice(1);
-}
