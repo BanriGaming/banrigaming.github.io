434b8884d85582c5078a053469fc57d158205511
BanriGaming <monkguru.guardian@gmail.com>
2023-09-15 11:01:49 -0500

    Update 3: Spacesuit.js Removed Progress Bar

--- assets/js/spacesuits.js
+++ assets/js/spacesuits.js
@@ -9,18 +9,9 @@ fetch('../assets/json/spacesuits.json')
         console.error('Error loading spacesuit set data:', error);
     });
 
-    // Function to update the progress bar
-function updateProgressBar(completed, total) {
-    const progressBar = document.getElementById('progress-bar');
-    const percentage = total > 0 ? (completed / total) * 100 : 0;
-    progressBar.style.width = percentage + '%';
-}
-
 // Function to generate the checklist dynamically
 function generateChecklist(spacesuitSets) {
     const columns = document.querySelectorAll('.column');
-    let totalItems = 0;
-    let completedItems = 0;
 
     spacesuitSets.forEach((set, index) => {
         const setHeader = document.createElement('h2');
@@ -30,25 +21,19 @@ function generateChecklist(spacesuitSets) {
 
         // Function to toggle completion status of all pieces in the set
         function toggleSetCompletion(event) {
-            if (event.target !== checkbox) {
-                const setPieces = document.querySelectorAll(`#${set.name.replace(/\s+/g, '').toLowerCase()}Piece1,
-                                                     #${set.name.replace(/\s+/g, '').toLowerCase()}Piece2,
-                                                     #${set.name.replace(/\s+/g, '').toLowerCase()}Piece3`);
-
-                setPieces.forEach(piece => {
-                    const checkbox = piece.previousElementSibling;
-                    const label = piece.nextElementSibling;
-                    if (!checkbox.checked) {
-                        checkbox.checked = true;
-                        label.className = 'completed';
-                        completedItems++;
-                        localStorage.setItem(piece.id, 'true'); // Save completion status
-                    }
-                });
-
-                // Update progress
-                updateProgressBar(completedItems, totalItems);
-            }
+            const setPieces = document.querySelectorAll(`#${set.name.replace(/\s+/g, '').toLowerCase()}Piece1,
+                                                         #${set.name.replace(/\s+/g, '').toLowerCase()}Piece2,
+                                                         #${set.name.replace(/\s+/g, '').toLowerCase()}Piece3`);
+        
+            setPieces.forEach(piece => {
+                const checkbox = piece.previousElementSibling;
+                const label = piece.nextElementSibling;
+                if (!checkbox.checked) {
+                    checkbox.checked = true;
+                    label.className = 'completed';
+                    localStorage.setItem(piece.id, 'true'); // Save completion status
+                }
+            });
         }
 
         // Add click event listener to the set title
@@ -80,22 +65,13 @@ function generateChecklist(spacesuitSets) {
             checkbox.addEventListener('change', function () {
                 label.className = checkbox.checked ? 'completed' : 'uncompleted';
 
-                // Update progress
-                if (checkbox.checked) {
-                    completedItems++;
-                    localStorage.setItem(pieceId, 'true');
-                } else {
-                    completedItems--;
-                    localStorage.setItem(pieceId, 'false');
-                }
-                updateProgressBar(completedItems, totalItems);
+                // Update completion status in local storage
+                localStorage.setItem(pieceId, checkbox.checked ? 'true' : 'false');
             });
 
             listItem.appendChild(checkbox);
             listItem.appendChild(label);
             setList.appendChild(listItem);
-
-            totalItems++; // Increment total items
         });
 
         // Distribute sets across columns
@@ -103,28 +79,15 @@ function generateChecklist(spacesuitSets) {
         column.appendChild(setHeader);
         column.appendChild(setList);
     });
-
-    // Update progress bar initially
-    updateProgressBar(completedItems, totalItems);
 }
 
 // Generate the checklist when the page loads
 window.addEventListener('load', () => {
-    fetch('../assets/json/spacesuits.json')
+    fetch('assets/json/spacesuits.json')
         .then(response => response.json())
         .then(data => {
             // Once data is loaded, generate the checklist
             generateChecklist(data);
-
-            // Update progress bar based on completion data from local storage
-            const checkboxes = document.querySelectorAll('input[type="checkbox"]');
-            checkboxes.forEach(checkbox => {
-                checkbox.addEventListener('change', function () {
-                    const checkedCheckboxes = document.querySelectorAll('input[type="checkbox"]:checked');
-                    const completedItems = checkedCheckboxes.length;
-                    updateProgressBar(completedItems, checkboxes.length);
-                });
-            });
         })
         .catch(error => {
             console.error('Error loading spacesuit set data:', error);
