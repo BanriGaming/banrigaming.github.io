71736b1c56fb9d5664fd2a915e68daf0dd033053
BanriGaming <monkguru.guardian@gmail.com>
2023-09-18 11:06:50 -0500

    Update 11: Tracker hotfix 1.1

--- assets/js/sfweapons.js
+++ assets/js/sfweapons.js
@@ -1,93 +1,99 @@
-// Load weapon data from the JSON file
-fetch('https://banrigaming.github.io/assets/json/sfweapons.json')
-    .then(response => response.json())
-    .then(data => {
-        generateWeaponList(data); // Generate the checklist
-    })
-    .catch(error => {
-        console.error('Error loading weapon data:', error);
-    });
+// Load the weapon data from the JSON file
+function fetchWeaponData() {
+    return fetch('https://banrigaming.github.io/assets/json/sfweapons.json') // Replace with the correct path to your JSON file
+        .then(response => response.json());
+}
 
 // Function to generate the weapon checklist dynamically
-function generateWeaponList(weaponsData) {
+function generateChecklist(weaponData) {
     const weaponList = document.getElementById('weapon-list');
-    const filterButtons = document.querySelectorAll('.filter-buttons button');
-    const savedChecks = JSON.parse(localStorage.getItem('weaponChecklist')) || {};
+    const categoryButtons = document.querySelectorAll('.filter-buttons button');
 
-    // Clear existing checklist
-    weaponList.innerHTML = '';
-
-    weaponsData.forEach(category => {
-        category.weapons.forEach(weapon => {
-            const weaponItem = document.createElement('li');
-            const checkbox = document.createElement('input');
-            const label = document.createElement('label');
-            checkbox.type = 'checkbox';
-            checkbox.id = weapon.name;
-
-            // Set the checkbox state based on local storage
-            if (savedChecks[weapon.name]) {
-                checkbox.checked = true;
-            }
-
-            label.textContent = weapon.name;
-            label.setAttribute('for', weapon.name);
-
-            // Append checkbox and label to the weapon item
-            weaponItem.appendChild(checkbox);
-            weaponItem.appendChild(label);
-
-            // Append the weapon item to the list
-            weaponList.appendChild(weaponItem);
-        });
-    });
-
-    // Add event listeners to filter buttons
-    filterButtons.forEach(button => {
-        button.addEventListener('click', () => {
-            const category = button.id;
-            filterWeapons(category);
-        });
-    });
-
-    // Initially, show all weapons
-    filterWeapons('all');
-}
-
-// Function to filter weapons by category
-function filterWeapons(category) {
-    const checkboxes = document.querySelectorAll('#weapon-list input');
-    checkboxes.forEach(checkbox => {
-        const weaponCategory = getWeaponCategory(checkbox.id);
-        if (category === 'all' || category === weaponCategory) {
-            checkbox.parentElement.style.display = 'block';
-        } else {
-            checkbox.parentElement.style.display = 'none';
-        }
-    });
-}
-
-// Function to get the category of a weapon
-function getWeaponCategory(weaponName) {
-    for (const category of weaponData) {
-        const foundWeapon = category.weapons.find(weapon => weapon.name === weaponName);
-        if (foundWeapon) {
-            return category.category;
-        }
+    // Function to clear the existing checklist
+    function clearChecklist() {
+        weaponList.innerHTML = '';
     }
-    return '';
+
+    categoryButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            // Remove active class from all buttons
+            categoryButtons.forEach(btn => btn.classList.remove('active'));
+            // Add active class to the clicked button
+            button.classList.add('active');
+
+            // Clear the existing checklist
+            clearChecklist();
+
+            // Filter weapons based on the clicked category
+            const category = button.id.toLowerCase();
+
+            if (category === 'all') {
+                // Display all weapons for the "All" category
+                weaponData.forEach(categoryWeapons => {
+                    const weaponRow = document.createElement('div');
+                    weaponRow.className = 'row';
+
+                    categoryWeapons.weapons.forEach(weapon => {
+                        const weaponItem = document.createElement('div');
+                        weaponItem.className = 'col-md-3'; // Use Bootstrap's grid classes to create 4 columns
+
+                        // Create an <h2> element for the weapon title with underlining
+                        const weaponTitle = document.createElement('h2');
+                        weaponTitle.innerHTML = `<u>${weapon.name}</u>`; // Display the weapon name with underlining
+                        weaponItem.appendChild(weaponTitle);
+
+                        // Create an image element and set its source
+                        const weaponImage = document.createElement('img');
+                        weaponImage.src = weapon.image; // Assuming your JSON data has the image URL
+                        weaponImage.alt = weapon.name; // Set alt text for accessibility
+                        weaponItem.appendChild(weaponImage);
+
+                        weaponRow.appendChild(weaponItem);
+                    });
+
+                    weaponList.appendChild(weaponRow);
+                });
+            } else {
+                // Display weapons for the selected category
+                const weapons = weaponData.find(item => item.category.toLowerCase() === category);
+
+                if (weapons) {
+                    const weaponRow = document.createElement('div');
+                    weaponRow.className = 'row';
+
+                    weapons.weapons.forEach(weapon => {
+                        const weaponItem = document.createElement('div');
+                        weaponItem.className = 'col-md-3'; // Use Bootstrap's grid classes to create 4 columns
+
+                        // Create an <h2> element for the weapon title with underlining
+                        const weaponTitle = document.createElement('h2');
+                        weaponTitle.innerHTML = `<u>${weapon.name}</u>`; // Display the weapon name with underlining
+                        weaponItem.appendChild(weaponTitle);
+
+                        // Create an image element and set its source
+                        const weaponImage = document.createElement('img');
+                        weaponImage.src = weapon.image; // Assuming your JSON data has the image URL
+                        weaponImage.alt = weapon.name; // Set alt text for accessibility
+                        weaponItem.appendChild(weaponImage);
+
+                        weaponRow.appendChild(weaponItem);
+                    });
+
+                    weaponList.appendChild(weaponRow);
+                }
+            }
+        });
+    });
 }
 
-// Save checkbox states to local storage on change
-const checkboxes = document.querySelectorAll('#weapon-list input');
-checkboxes.forEach(checkbox => {
-    checkbox.addEventListener('change', () => {
-        const weaponName = checkbox.id;
-        const isChecked = checkbox.checked;
-
-        // Save the checkbox state to local storage
-        const savedChecks = JSON.parse(localStorage.getItem('weaponChecklist')) || {};
-        savedChecks[weaponName] = isChecked;
-        localStorage.setItem('weaponChecklist', JSON.stringify(savedChecks));
-    });
+// Generate the checklist for the default category on page load
+window.addEventListener('load', () => {
+    fetchWeaponData()
+        .then(data => {
+            // Once data is loaded, generate the checklist
+            generateChecklist(data);
+        })
+        .catch(error => {
+            console.error('Error loading weapon data:', error);
+        });
 });
