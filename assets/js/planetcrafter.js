d24ceb035963256cfd6b842c031ee09e0884b86d
BanriGaming <monkguru.guardian@gmail.com>
2023-11-13 20:11:05 -0600

    Update 27: Beginning Planet Crafter Project

---
+++ assets/js/planetcrafter.js
@@ -0,0 +1,92 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const select = document.getElementById('buildings');
+    const shoppingList = document.getElementById('shoppingList');
+    const generateButton = document.getElementById('generateButton');
+
+    let buildingsData;
+    let materialsData;
+
+    // Fetch data from JSON files
+    fetch('assets/json/pcbuildings.json')
+        .then(response => response.json())
+        .then(data => {
+            buildingsData = data;
+            populateDropdown(data);
+        });
+
+    fetch('assets/json/pcmaterials.json')
+        .then(response => response.json())
+        .then(data => {
+            materialsData = data;
+        });
+
+    function populateDropdown(data) {
+        for (const building in data) {
+            const option = document.createElement('option');
+            option.value = building;
+            option.textContent = building;
+            select.appendChild(option);
+        }
+    }
+
+    generateButton.addEventListener('click', function () {
+        const selectedBuilding = select.value;
+        const quantity = document.getElementById('quantity').valueAsNumber;
+
+        const buildingData = buildingsData[selectedBuilding];
+
+        if (buildingData) {
+            const basicMaterials = materialsData['basicMaterials'];
+            const totalMaterials = { ...materialsData['advancedMaterials'][selectedBuilding] };
+
+            for (const material in totalMaterials) {
+                totalMaterials[material] *= quantity;
+                if (basicMaterials.hasOwnProperty(material)) {
+                    basicMaterials[material] = totalMaterials[material];
+                    delete totalMaterials[material];
+                }
+            }
+
+            displayShoppingList(basicMaterials, totalMaterials, quantity, buildingData);
+        }
+    });
+
+    function displayShoppingList(basicMaterials, totalMaterials, quantity, buildingData) {
+        shoppingList.innerHTML = ''; // Clear previous content
+
+        const listHeader = document.createElement('h3');
+        listHeader.textContent = 'Shopping List:';
+        shoppingList.appendChild(listHeader);
+
+        const basicMaterialsHeader = document.createElement('p');
+        basicMaterialsHeader.textContent = 'Basic Materials:';
+        shoppingList.appendChild(basicMaterialsHeader);
+
+        for (const material in basicMaterials) {
+            const materialItem = document.createElement('p');
+            materialItem.textContent = `${material}: ${basicMaterials[material]} units`;
+            shoppingList.appendChild(materialItem);
+        }
+
+        const advancedMaterialsHeader = document.createElement('p');
+        advancedMaterialsHeader.textContent = 'Advanced Materials:';
+        shoppingList.appendChild(advancedMaterialsHeader);
+
+        for (const material in totalMaterials) {
+            const materialItem = document.createElement('p');
+            materialItem.textContent = `${material}: ${totalMaterials[material]} units`;
+            shoppingList.appendChild(materialItem);
+        }
+
+        if (buildingData['PowerUsage']) {
+            const powerUsage = document.createElement('p');
+            powerUsage.textContent = `Total Power Usage: ${buildingData['PowerUsage'] * quantity}`;
+            shoppingList.appendChild(powerUsage);
+        }
+        if (buildingData['Production']) {
+            const production = document.createElement('p');
+            production.textContent = `Total Production: ${buildingData['Production'] * quantity}`;
+            shoppingList.appendChild(production);
+        }
+    }
+});
