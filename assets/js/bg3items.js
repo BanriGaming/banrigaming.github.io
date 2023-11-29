d1d12de695963c202e3fc5bab26466f33199e75d
BanriGaming <monkguru.guardian@gmail.com>
2023-11-29 15:44:19 -0600

    Update 36: Fixed my BG3 ID tool

--- assets/js/bg3items.js
+++ assets/js/bg3items.js
@@ -1,84 +1,77 @@
 // Function to fetch and merge data from multiple JSON files
 async function fetchData() {
-    let items = [];
-  
-    // Fetch data from items1.json
-    await fetch('https://banrigaming.github.io/assets/json/bg3items1.json')
-      .then(response => response.json())
-      .then(data => {
-        items = items.concat(data.items);
-      })
-      .catch(error => console.error('Error fetching items1:', error));
-  
-    // Fetch data from items2.json
-    await fetch('https://banrigaming.github.io/assets/json/bg3items2.json')
-      .then(response => response.json())
-      .then(data => {
-        items = items.concat(data.items);
-      })
-      .catch(error => console.error('Error fetching items2:', error));
-  
-    // Fetch data from items3.json
-    await fetch('https://banrigaming.github.io/assets/json/bg3items3.json')
-      .then(response => response.json())
-      .then(data => {
-        items = items.concat(data.items);
-      })
-      .catch(error => console.error('Error fetching items3:', error));
-  
-    return items;
-  }
-  
-  // Function to copy item ID to clipboard and update button text
-  function copyToClipboardAndUpdateButton(button, text) {
-    const el = document.createElement('textarea');
-    el.value = text;
-    document.body.appendChild(el);
-    el.select();
-    document.execCommand('copy');
-    document.body.removeChild(el);
-  
-    button.textContent = 'Copied!'; // Update button text to indicate it's copied
-    setTimeout(() => {
-      button.textContent = 'Copy'; // Revert button text after 1.5 seconds
-    }, 1500);
-  }
-  
-  // Function to display filtered items
-  function displayItems(filteredItems) {
-    const itemsDiv = document.getElementById('items');
-    itemsDiv.innerHTML = '';
-  
-    filteredItems.forEach(item => {
-      const div = document.createElement('div');
-      const button = document.createElement('button');
-      button.textContent = 'Copy';
-      button.addEventListener('click', () => {
-        copyToClipboardAndUpdateButton(button, item.id);
-      });
-  
-      div.innerHTML = `${item.name} - ID: ${item.id} `;
-      div.appendChild(button);
-      itemsDiv.appendChild(div);
+  let items = [];
+
+  // Fetch data from items1.json
+  const response1 = await fetch('https://banrigaming.github.io/assets/json/bg3items1.json');
+  const data1 = await response1.json();
+  items = items.concat(data1.items);
+
+  // Fetch data from items2.json
+  const response2 = await fetch('https://banrigaming.github.io/assets/json/bg3items2.json');
+  const data2 = await response2.json();
+  items = items.concat(data2.items);
+
+  // Fetch data from items3.json
+  const response3 = await fetch('https://banrigaming.github.io/assets/json/bg3items3.json');
+  const data3 = await response3.json();
+  items = items.concat(data3.items);
+
+  return items;
+}
+
+// Function to copy item ID to clipboard and update button text
+function copyToClipboardAndUpdateButton(button, text) {
+  const el = document.createElement('textarea');
+  el.value = text;
+  document.body.appendChild(el);
+  el.select();
+  document.execCommand('copy');
+  document.body.removeChild(el);
+
+  button.textContent = 'Copied!'; // Update button text to indicate it's copied
+  setTimeout(() => {
+    button.textContent = 'Copy'; // Revert button text after 1.5 seconds
+  }, 1500);
+}
+
+// Function to display filtered items
+function displayItems(filteredItems) {
+  const itemsDiv = document.getElementById('items');
+  itemsDiv.innerHTML = '';
+
+  filteredItems.forEach(item => {
+    const div = document.createElement('div');
+    const button = document.createElement('button');
+    button.textContent = 'Copy';
+    button.addEventListener('click', () => {
+      copyToClipboardAndUpdateButton(button, item.id);
     });
+
+    div.innerHTML = `${item.name} - ID: ${item.id} `;
+    div.appendChild(button);
+    itemsDiv.appendChild(div);
+  });
+}
+
+// Function to search items
+async function searchItems() {
+  const searchTerm = document.getElementById('search').value.toLowerCase();
+  const allItems = await fetchData(); // Fetch all items
+
+  const filteredItems = allItems.filter(item => item.name.toLowerCase().includes(searchTerm));
+  displayItems(filteredItems);
+
+  const itemsDiv = document.getElementById('items');
+  if (searchTerm.trim() === '') {
+    itemsDiv.style.display = 'none'; // Hide all items if search term is empty
+  } else {
+    itemsDiv.style.display = 'block'; // Show items if search term is entered
   }
-  
-  // Function to search items
-  async function searchItems() {
-    const searchTerm = document.getElementById('search').value.toLowerCase();
-    const allItems = await fetchData(); // Fetch all items
-  
-    if (searchTerm.trim() !== '') {
-      const filteredItems = allItems.filter(item => item.name.toLowerCase().includes(searchTerm));
-      displayItems(filteredItems);
-    } else {
-      document.getElementById('items').innerHTML = ''; // Clear items if search term is empty
-    }
-  }
-  
-  // Event listener for search input
-  document.getElementById('search').addEventListener('input', searchItems);
-  
-  // Initial display of all items
-  fetchData().then(allItems => displayItems(allItems));
-  
+}
+
+// Event listener for search input
+document.getElementById('search').addEventListener('input', searchItems);
+
+// Initially hide all items
+document.getElementById('items').style.display = 'none';
